6ac7bf9fc6b8db3d38552463a7074d1c8507999a
rogeriop <pinheiroxrogerio@gmail.com>
2019-12-02 21:25:11 -0300

    Curso devReactJs #18.09 - Carregando os dados de produtos do servidor

--- src/Categoria.js
+++ src/Categoria.js
@@ -1,8 +1,54 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+
 
 class Categoria extends Component {
+    constructor (props) {
+        super(props)
+        this.loadData = this.loadData.bind(this)
+        this.state = {
+            produtos: [],
+            categoria: {}
+        }
+    }
+    loadData(id) {
+      axios
+        .get('http://localhost:3001/produtos?categoria='+id)
+        .then(res => {
+            this.setState({
+                produtos: res.data
+            })
+        })
+      axios
+        .get('http://localhost:3001/categorias/'+id)
+        .then(res => {
+            this.setState({
+                categoria: res.data
+            })
+        })
+    
+    }
+    // quando o componente for montado
+    componentDidMount() {
+        const id = this.props.match.params.catId
+        this.loadData(id)
+    }
+    componentWillReceiveProps(newProps) {
+        this.loadData(newProps.match.params.catId)
+    }
+    renderProduto(produto) {
+        return (
+            <p className='well' key={produto.id}>{produto.produto}</p>
+        )
+    }
     render () {
-        return <h1>Categoria {this.props.match.params.catId} </h1>
+        return (
+            <div>
+                <h1>{this.state.categoria.categoria} </h1>
+                {this.state.produtos.map(this.renderProduto)}
+
+                </div>
+        )
     }
 }
 export default Categoria
